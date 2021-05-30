419d3056f8581006acbb2ca8f6bd8a62d190c8a2
momos2-app <lsy1021302@naver.com>
2021-05-30 23:47:11 +0900

    아무것도 안함

--- app/src/main/java/com/example/test/Fragment2.java
+++ app/src/main/java/com/example/test/Fragment2.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Fragment2 extends Fragment {
 
     Button addButton;
+
+
+
 
     private View view;
 
@@ -28,5 +34,5 @@ public class Fragment2 extends Fragment {
     }
 
 
-
 }
+
